9aabb8fcf9d7ef11bbfe81e4e82da75b58bcf8fc
John Tantillo <johntantillo1@gmail.com>
2021-09-28 14:20:33 -0400

    Adding heroku.yml with file changes for deployment

--- Dockerfile
+++ Dockerfile
@@ -18,9 +18,9 @@ COPY . .
 
 RUN pip install -r requirements.txt
 
-EXPOSE 8000
+EXPOSE $PORT
 
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
 RUN chmod +x /wait
-CMD /wait && python -u app.py
+CMD /wait && python app.py $PORT
 
--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/') 
 def index():
-    total_viewers = MongoBase.Update_Statistics("Total_Users", 1)
-    return 'Hello World! (CSE 442 Team Placeholder) there has been ' + str(total_viewers) + ' total viewers of the website'
+    # total_viewers = MongoBase.Update_Statistics("Total_Users", 1)
+    return 'Hello World! (CSE 442 Team Placeholder) there has been '
 
-app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    print(port)
+    app.run(host="0.0.0.0", port=port)
