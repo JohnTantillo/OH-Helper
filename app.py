7c91dcf0a72224161fcf605964bc8746dc2d3cef
smalinow <smalinow@buffalo.edu>
2021-10-01 00:47:07 -0400

    Heroku, 10/1 In class assignment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     total_viewers = MongoBase.Update_Statistics("Total_Users", 1)
-    return "Hello World! (CSE 442 Team Placeholder) there has been " + total_viewers + " to the site"
+    return "Hello World! (CSE 442 Team Placeholder) there has been " + str(total_viewers) + " to the site"
 
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
