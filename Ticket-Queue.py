39ef108962e00925d3201d89b440faa1b1279ee7
John Tantillo <johntantillo1@gmail.com>
2021-10-31 17:58:38 -0400

    Added insert and remove functions for pqueue

--- Ticket-Queue.py
+++ Ticket-Queue.py
@@ -1,3 +1,5 @@
+from queue import Queue 
+
 class Ticket:
     def __init__(self, prios, name, message):
         self.prios = prios
@@ -13,8 +15,26 @@ class Ticket:
     def get_message(self):
         return self.message
 
+class PriorityQueue:
+    def __init__(self, q):
+        self.q = [Queue()]*4
+
+    def insert(self, tic):
+        priority = tic.get_prios()
+        ticket_queue = self.q[priority]
+        ticket_queue.put(tic)
+
+    def admit_next(self):
+        ind = 0
+        while self.q[ind].empty():
+            ind += 1
+        return self.q[ind].get()
+
 if __name__ == "__main__":
     tix = Ticket(1, 'Johntant', 'This is a test')
-    print(tix.get_prios())
-    print(tix.get_name())
-    print(tix.get_message())
+    tix2 = Ticket(0, 'Me', 'This should be the correct message')
+    print(tix2.get_prios())
+    pq = PriorityQueue([])
+    pq.insert(tix)
+    pq.insert(tix2)
+    print(pq.admit_next().get_message())    
