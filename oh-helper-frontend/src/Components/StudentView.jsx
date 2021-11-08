fcf36197ec010cd40b5ed2e9ce47492a6dd73532
John K Dunaske <j_dunaske@yahoo.com>
2021-11-07 21:20:48 -0500

    Heroku Hot Fix #10000

--- oh-helper-frontend/src/Components/StudentView.jsx
+++ oh-helper-frontend/src/Components/StudentView.jsx
@@ -21,7 +21,7 @@ class StudentView extends React.Component {
   }
 
   componentDidMount() {
-    socket = new WebSocket("ws://team-placeholder-oh.herokuapp.com/websocket");
+    socket = new WebSocket("wss://team-placeholder-oh.herokuapp.com/websocket");
 
     socket.addEventListener("open", (event) => {
       console.log("Websocket Connected!");
